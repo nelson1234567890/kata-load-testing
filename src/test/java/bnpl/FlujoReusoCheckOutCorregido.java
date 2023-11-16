1b74e980ed5fdfca49cdc43447b16331fb3b5574
nroja13 <nroja13@bancodebogota.com.co>
2023-11-16 15:46:19 -0500

    pruebas de carga para BNPL1

--- src/test/java/bnpl/FlujoReusoCheckOutCorregido.java
+++ src/test/java/bnpl/FlujoReusoCheckOutCorregido.java
@@ -15,13 +15,24 @@ import static io.gatling.javaapi.http.HttpDsl.http;
 
 public class FlujoReusoCheckOutCorregido extends Simulation {
 
+
+    private String content = "Content-Type";
+    private String aplication = "application/json";
+    private String request = "X-RqUID";
+    private String requestVal = "3e4f32c2-82be-405a-8213-695789068032";
+    private String ipAddres = "X-IPAddr";
+    private String ipAddresVal = "127.0.0.1";
+    private String custID = "X-IdentSerialNum";
+    private String custIDType = "X-CustIdentType";
+
+
     private HttpProtocolBuilder httpProtocol = http
             .baseUrl("https://api-bnpl.labdigbdbstgae.com");
     private Map<CharSequence, String> allied = Map.ofEntries(
-            Map.entry("Content-Type", "application/json"),
-            Map.entry("X-RqUID", "3e4f32c2-82be-405a-8213-695789068032"),
+            Map.entry(content, aplication),
+            Map.entry(request, requestVal),
             Map.entry("X-alliedToken", "02ab6ea5-54b5-4bcb-b145-fcd442148f57"),
-            Map.entry("X-IPAddr", "127.0.0.1"),
+            Map.entry(ipAddres, ipAddresVal),
             Map.entry("X-IdentType", "CC"),
             Map.entry("X-amount", "2000000"),
             Map.entry("X-salepoint", "Punto de venta Bogota"),
@@ -29,44 +40,44 @@ public class FlujoReusoCheckOutCorregido extends Simulation {
             Map.entry("X-channel", "Web")
     );
     private Map<CharSequence, String> token = Map.ofEntries(
-            Map.entry("Content-Type", "application/json"),
-            Map.entry("X-IPAddr", "127.0.0.1"),
-            Map.entry("X-RqUID", "3e4f32c2-82be-405a-8213-695789068032"),
-            Map.entry("X-IdentSerialNum", "123454667"),
-            Map.entry("X-CustIdentType", "CC")
+            Map.entry(content, aplication),
+            Map.entry(ipAddres, ipAddresVal),
+            Map.entry(request, requestVal),
+            Map.entry(custID, "123454667"),
+            Map.entry(custIDType, "CC")
     );
 
 
     private Map<CharSequence, String> bankfees = Map.ofEntries(
-            Map.entry("Content-Type", "application/json"),
-            Map.entry("X-IPAddr", "127.0.0.1"),
-            Map.entry("X-IdentSerialNum", "123454667")
+            Map.entry(content, aplication),
+            Map.entry(ipAddres, ipAddresVal),
+            Map.entry(custID, "123454668")
     );
 
     private Map<CharSequence, String> products = Map.ofEntries(
-            Map.entry("Content-Type", "application/json"),
-            Map.entry("X-RqUID", "3e4f32c2-82be-405a-8213-695789068032"),
-            Map.entry("X-IPAddr", "127.0.0.1"),
-            Map.entry("X-CustIdentType", "CC"),
+            Map.entry(content, aplication),
+            Map.entry(request, requestVal),
+            Map.entry(ipAddres, ipAddresVal),
+            Map.entry(custIDType, "CC"),
             Map.entry("X-CustIdentNum", "123454667"),
             Map.entry("X-channel", "Web")
     );
 
 
     private Map<CharSequence, String> bankfeeswithcreditcardamount = Map.ofEntries(
-            Map.entry("Content-Type", "application/json"),
-            Map.entry("X-IPAddr", "127.0.0.1"),
-            Map.entry("X-RqUID", "3e4f32c2-82be-405a-8213-695789068032"),
-            Map.entry("X-IdentSerialNum", "1000117217"),
+            Map.entry(content, aplication),
+            Map.entry(ipAddres, ipAddresVal),
+            Map.entry(request, requestVal),
+            Map.entry(custID, "1000117219"),
             Map.entry("X-Journey", "Checkout")
     );
     private Map<CharSequence, String> closeDigReq = Map.ofEntries(
-            Map.entry("Content-Type", "application/json"),
-            Map.entry("X-IPAddr", "127.0.0.1"),
-            Map.entry("X-CustIdentType", "CC"),
+            Map.entry(content, aplication),
+            Map.entry(ipAddres, ipAddresVal),
+            Map.entry(custIDType, "CC"),
             Map.entry("X-CustIdentNum", "1000117217"),
             Map.entry("X-IsClient", "NO_CLIENT"),
-            Map.entry("X-RqUID", "3e4f32c2-82be-405a-8213-695789068032"),
+            Map.entry(request, requestVal),
             Map.entry("X-digRequestId", "111")
     );
 
