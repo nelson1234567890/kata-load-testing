ceda70cee375baf82cbbb799d406a04779002adb
nroja13 <nroja13@bancodebogota.com.co>
2023-11-16 15:29:48 -0500

    pruebas de carga para BNPL

--- src/test/java/bnpl/Engine.java
+++ src/test/java/bnpl/Engine.java
@@ -2,17 +2,19 @@ package bnpl;
 
 import io.gatling.app.Gatling;
 import io.gatling.core.config.GatlingPropertiesBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class Engine {
+
+public final class Engine {
 
   public static void main(String[] args) {
     GatlingPropertiesBuilder props = new GatlingPropertiesBuilder()
-      .resourcesDirectory(IDEPathHelper.mavenResourcesDirectory.toString())
-      .resultsDirectory(IDEPathHelper.resultsDirectory.toString())
-      .binariesDirectory(IDEPathHelper.mavenBinariesDirectory.toString());
+      .resourcesDirectory(IDEPathHelper.MAVENRESOURCESDIRECTORY.toString())
+      .resultsDirectory(IDEPathHelper.RESULTSDIRECTORY.toString())
+      .binariesDirectory(IDEPathHelper.MAVENBINARIESDIRECTORY.toString());
 
     Gatling.fromMap(props.build());
+  }
+  private Engine() {
+    //not called
   }
 }
